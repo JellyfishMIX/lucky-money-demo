6cdda9dd474956cad2c5a952ffd5e55ac79476d1
JellyfishMIX <2273516339@qq.com>
2019-12-23 07:53:05 +0800

    :see_no_evil: Update the .gitignore file

--- src/main/java/com/imooc/luckymoney/HelloController.java
+++ src/main/java/com/imooc/luckymoney/HelloController.java
@@ -1,19 +1,18 @@
 package com.imooc.luckymoney;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.math.BigDecimal;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/hello")
 public class HelloController {
     @Autowired
     private LimitConfig limitConfig;
 
-    @GetMapping("/hello")
-    public String say() {
-        return "Description: " + limitConfig.getDescription();
+    // @GetMapping("/hi")
+    @GetMapping("/say")
+    public String say(@RequestParam(value = "id", required = false, defaultValue = "0") Integer id) {
+        // return "Description: " + limitConfig.getDescription();
+        return "id:" + id;
     }
 }
