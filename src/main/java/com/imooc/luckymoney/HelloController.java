68340452548f8911cafee57b1bc861e3aa13921e
JellyfishMIX <2273516339@qq.com>
2019-12-22 19:29:49 +0800

    :construction: Add application-dev.yml and application-product.yml

--- src/main/java/com/imooc/luckymoney/HelloController.java
+++ src/main/java/com/imooc/luckymoney/HelloController.java
@@ -1,15 +1,19 @@
 package com.imooc.luckymoney;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigDecimal;
 
 @RestController
 public class HelloController {
+    @Autowired
+    private LimitConfig limitConfig;
+
     @GetMapping("/hello")
     public String say() {
-        return "跟慕课网廖师兄学SpringBoot!";
+        return "Description: " + limitConfig.getDescription();
     }
 }
